343aad7768ed766fb011bd9571113ba7ee0a063b
Becca Randall <becca.m.randall@gmail.com>
2019-01-11 22:33:30 -0600

    Added schema

--- models/album.js
+++ models/album.js
@@ -2,10 +2,12 @@
 module.exports = (sequelize, DataTypes) => {
   const album = sequelize.define('album', {
     title: DataTypes.STRING,
-    album - art: DataTypes.STRING
+    album_art: DataTypes.STRING
   }, {});
   album.associate = function(models) {
     // associations can be defined here
+    album.belongsToMany(models.song, { through: 'album_song', foreignKey: 'album_id'});
+
   };
   return album;
 };
