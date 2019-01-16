f7f65b4fd5132d72881c7d48022780749f5e1125
KimjManansala <kimjmanansala@gmail.com>
2019-01-15 23:56:44 -0600

    Fixed another bug

--- routes/artist.js
+++ routes/artist.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const db = require('../models')
 
-db.artist.findAll().then(res, function {
+db.artist.findAll().then(function (res){
     console.log(res)
 })
 
