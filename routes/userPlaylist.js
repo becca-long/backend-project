29ef5e1846cf0bf4cdb456dbadd2edabe1300664
Lee Arnold <lee.arnoldiii@gmail.com>
2019-01-19 11:49:36 -0600

    created layout in dummy file to delete later. will update layout in ejs files after push.

--- routes/userPlaylist.js
+++ routes/userPlaylist.js
@@ -2,13 +2,15 @@ const express = require('express')
 const router = express.Router()
 const db = require('../models/')
 
-
-db.playlist.findAll().then(function(res) {
-    // console.log(res)
-
-})
-
-// router.get('/dashboard', (req, res) => {
-
+// db.playlist.findAll().then(function (res) {
+//   console.log(res)
 // })
+
+router.get('/userPlaylist', (req, res) => {
+  res.render('index', {
+    pageTitle: 'playlist',
+    pageID: 'playlist',
+    data: 'hello'
+  })
+})
 module.exports = router
