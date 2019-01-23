932dac4d824181a1108a81845de9c4f4ec05d90d
Lee Arnold <lee.arnoldiii@gmail.com>
2019-01-22 19:57:38 -0600

    updated files to start process of creating render for user playlist. Will update again after recent pull and learning stuff from Queen E.

--- routes/createPlaylist.js
+++ routes/createPlaylist.js
@@ -11,17 +11,17 @@ router.use(bodyParser.urlencoded({
 router.post('/api/playlist', createPlaylistRoute)
 
 function createPlaylistRoute (req, res, next) {
-    console.log(req.body.title)
-    var playlistTitle = req.body.title
-    //To Do: replace userId with correct identifier that will pull in logged in user's id ('req.session.user.id' ?)
-    createPlaylist(userId, playlistTitle)
-    //To Do: Add in catch function with error handling
+  console.log(req.body.title)
+  var playlistTitle = req.body.title
+  const userId = req.session.user.id
+  createPlaylist(userId, playlistTitle)
+    // To Do: Add in catch function with error handling
     .catch()
     .then((result) => {
-        console.log('success')
-        console.log(result)
-        res.redirect('/userPlaylist?added=true')
-        res.end()
+      console.log('success')
+      console.log(result)
+      res.redirect('/userPlaylist?added=true')
+      res.end()
     })
 }
 
--- routes/userPlaylist.js
+++ routes/userPlaylist.js
@@ -14,4 +14,29 @@ router.get('/userPlaylist', (req, res) => {
   })
 })
 
+router.get('/findPlayList', (req, res) => {
+  db.playlist.findAll({
+    where: {
+      title: 'Demo Playlist 5'
+    }
+  }).then(results => {
+    let pId = results[0].dataValues.id
+    db.song.findAll({
+      // where: {
+      //   playlist_id: pId
+      // },
+      include: [{
+        model: db.playlist,
+        through: db.playlist_song,
+        required: true
+
+      }]
+    }).then(results => {
+      results.map((e) => {
+        console.log(e.dataValues.title)
+      })
+    })
+  })
+  res.render()
+})
 module.exports = router
